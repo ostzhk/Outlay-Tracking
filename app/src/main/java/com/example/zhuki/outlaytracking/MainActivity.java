d0b6f2fe0773938d5a9ee78b0ac3046ec2f6cc0e
ostzhk <ostzhk@mail.ru>
2016-07-24 20:33:15 +0600

    .

--- app/src/main/java/com/example/zhuki/outlaytracking/MainActivity.java
+++ app/src/main/java/com/example/zhuki/outlaytracking/MainActivity.java
@@ -74,5 +74,4 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, categoryList);
         dropdown.setAdapter(adapter);
     }
-    //Feature branch change
 }
