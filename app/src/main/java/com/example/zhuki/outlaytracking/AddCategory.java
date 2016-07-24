a071a0d454499e84d1c184d709f0f1fd21b79a21
ostzhk <ostzhk@mail.ru>
2016-07-24 21:03:17 +0600

    Test

--- app/src/main/java/com/example/zhuki/outlaytracking/AddCategory.java
+++ app/src/main/java/com/example/zhuki/outlaytracking/AddCategory.java
@@ -1,14 +1,12 @@
 package com.example.zhuki.outlaytracking;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.util.ArrayList;
 
 public class AddCategory extends Activity {
     EditText editText;
